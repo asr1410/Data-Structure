4a7c9030fd14672127056fa9298656604d77ee16
asr1410 <abhishek.rawat1410@gmail.com>
2021-01-17 23:46:04 +0530

    changes

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
+// A simple CPP program to introduce
+// a linked list
 #include <bits/stdc++.h>
-#include <cmath>
-#include <fstream>
-#include <string>
-#include <cstdio>
-#include <algorithm>
 using namespace std;
+
 class Node
 {
 public:
@@ -13,40 +10,102 @@ public:
     Node *next;
 };
 
-void printLinkedList(Node *head)
+// This function prints contents of linked list
+// starting from the given node
+void printList(Node *n)
 {
-    while (head != NULL)
+    while (n != NULL)
     {
-        cout << "The elements are " << head->data << endl;
-        head = head->next;
+        cout << n->data << " ";
+        n = n->next;
     }
 }
 
+// Program to create a simple linked
+// list with 3 nodes
 int main()
 {
     Node *head = NULL;
     Node *second = NULL;
-    Node *three = NULL;
-    Node *fourth = NULL;
+    Node *third = NULL;
 
+    // allocate 3 nodes in the heap
     head = new Node();
     second = new Node();
-    three = new Node();
-    fourth = new Node();
+    third = new Node();
 
-    head->data = 1;
-    head->next = second;
+    /* Three blocks have been allocated dynamically.  
+    We have pointers to these three blocks as head,  
+    second and third      
+    head         second         third  
+        |             |             |  
+        |             |             |  
+    +---+-----+     +----+----+     +----+----+  
+    | # | # |     | # | # |     | # | # |  
+    +---+-----+     +----+----+     +----+----+  
+      
+# represents any random value.  
+Data is random because we haven’t assigned  
+anything yet */
 
+    head->data = 1;      // assign data in first node
+    head->next = second; // Link first node with
+    // the second node
+
+    /* data has been assigned to the data part of first  
+    block (block pointed by the head). And next  
+    pointer of the first block points to second.  
+    So they both are linked.  
+  
+    head         second         third  
+        |             |             |  
+        |             |             |  
+    +---+---+     +----+----+     +-----+----+  
+    | 1 | o----->| # | # |     | # | # |  
+    +---+---+     +----+----+     +-----+----+      
+*/
+
+    // assign data to second node
     second->data = 2;
-    second->next = three;
 
-    three->data = 3;
-    three->next = fourth;
+    // Link second node with the third node
+    second->next = third;
 
-    fourth->data = 4;
-    fourth->next = NULL;
+    /* data has been assigned to the data part of the second  
+    block (block pointed by second). And next  
+    pointer of the second block points to the third  
+    block. So all three blocks are linked.  
+      
+    head         second         third  
+        |             |             |  
+        |             |             |  
+    +---+---+     +---+---+     +----+----+  
+    | 1 | o----->| 2 | o-----> | # | # |  
+    +---+---+     +---+---+     +----+----+     */
 
-    printLinkedList(head);
+    third->data = 3; // assign data to third node
+    third->next = NULL;
+
+    /* data has been assigned to the data part of the third  
+    block (block pointed by third). And next pointer  
+    of the third block is made NULL to indicate  
+    that the linked list is terminated here.  
+  
+    We have the linked list ready.  
+  
+        head      
+            |  
+            |  
+        +---+---+     +---+---+     +----+------+  
+        | 1 | o----->| 2 | o-----> | 3 | NULL |  
+        +---+---+     +---+---+     +----+------+      
+      
+      
+    Note that only the head is sufficient to represent  
+    the whole list. We can traverse the complete  
+    list by following the next pointers. */
+
+    printList(head);
 
     return 0;
 }
