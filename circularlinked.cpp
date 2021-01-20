2a24ed08621aeb9d84f7d303d3dc4bf0c7f45912
asr1410 <abhishek.rawat1410@gmail.com>
2021-01-20 13:01:56 +0530

    changes

--- circularlinked.cpp
+++ circularlinked.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+void push(Node *head)
+{
+    int value;
+    cin >> value;
+    Node *ptr = new Node();
+    ptr->data = value;
+    if (head != NULL)
+    {
+        }
+}
 int main()
 {
-
+    Node *head = NULL;
+    push(head);
     return 0;
 }
