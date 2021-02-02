46126e3e80de7bd61a9d6fae7e4a357055c642cc
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-02-02 11:52:15 +0530

    changes

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -11,6 +11,22 @@ void printArray(int *A, int n)
 }
 void selectionSort(int *arr, int n)
 {
+    cout << "running selection sort\n";
+    int indexOfMin, temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[indexOfMin])
+            {
+                indexOfMin = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[indexOfMin];
+        arr[indexOfMin] = temp;
+    }
 }
 int main()
 {
