8c99ceea9ae9712053499ff829e2e1d5019f8414
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-02-10 17:01:01 +0530

    changes

---
+++ STL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+// NOTES:
+// 1. STL standard template library.
+// 2. It consisit of three library.
+//     a.Container
+//     b.Iterator
+//     c.Algorithm
+int main()
+{
+    array<int, 5> myarray = {1, 2, 3, 4, 5}; //intialize list
+    array<int, 5> myarray2{1, 2, 3, 4, 5};   //uniform intialization
+    return 0;
+}
